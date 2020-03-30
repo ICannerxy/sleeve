3847cca3e9e9bb6f005667ac0adc6dd16068730d
xuyang <icanner@qq.com>
2020-03-30 23:05:40 +0800

    设置首页全局background颜色

--- model/theme.js
+++ model/theme.js
@@ -1,12 +1,21 @@
 import {Http} from "../utils/http";
 
 class Theme {
+    static locationA = 't-1'
+    static locationE = 't-2'
+
     static async getHomeLoactionA() {
         return await Http.request({
             url: 'theme/by/names',
             data: {
-                names: 't-1'
+                names: Theme.locationA
             }
+        })
+    }
+
+    static async getHomeLocationE() {
+        return await Http.request({
+            url: ``
         })
     }
 }
